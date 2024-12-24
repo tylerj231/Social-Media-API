4ecff43926ad1c95ced431a80aca14ac40329324
Mykola <jacktyler2391@gmail.com>
2024-12-24 14:10:13 +0200

    Create & register models

--- social_media/admin.py
+++ social_media/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from social_media.models import User, Post, Follow
+
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Follow)
--- social_media/models.py
+++ social_media/models.py
@@ -1,3 +1,45 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.text import slugify
+
+
+class User(AbstractUser):
+    bio = models.CharField(
+        max_length=300,
+        blank=True,
+        null=True
+    )
+    profile_picture = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to="user_profile_image_path"
+    )
+
+    def __str__(self):
+        return self.username
+
+    def user_profile_image_path(self, filename):
+        _, extension = os.path.splitext(filename)
+        return os.path.join(
+            "uploads/images/",
+            f"{slugify(self.username)}-{uuid.uuid4()}{extension}"
+        )
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=100,
+        unique=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(max_length=700)
+    users = models.ManyToManyField(User, related_name="posts")
+
+    def __str__(self):
+        return self.title
+
+class Follow(models.Model):
+    users = models.ManyToManyField(User, related_name="followers")
