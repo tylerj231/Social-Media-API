1663e9f657a906e0645112d4237ab1d3920305fa
Mykola <jacktyler2391@gmail.com>
2024-12-28 15:10:07 +0200

    Alter README.md file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Social-Media-API
+## Social-Media-API
+
+- RESTful API for basic social-media interactions.
+- API allows users to carry out basic social-media actions such as:
+create user & profile, create posts, follow other users.
--- SocialMediaAPI/settings.py
+++ SocialMediaAPI/settings.py
@@ -85,10 +85,6 @@ WSGI_APPLICATION = "SocialMediaAPI.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB"),
